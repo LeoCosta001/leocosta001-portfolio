89c4a122180cfe791612e528f558eb76c74cfd84
LeoCosta <leocosta.black@hotmail.com>
2023-12-27 17:53:26 -0300

    Install material-tailwind lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'tailwindcss'
+import withMT from '@material-tailwind/react/utils/withMT'
 
-const config: Config = {
+const config: object = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [],
-}
-export default config
+} as Config);
+
+export default config;
