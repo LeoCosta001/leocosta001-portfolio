228c37abc7e7896a1fa8b1697057e68978302988
LeoCosta <leocosta.black@hotmail.com>
2023-12-29 18:26:17 -0300

    Add primary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,20 @@
-import type { Config } from 'tailwindcss'
-import withMT from '@material-tailwind/react/utils/withMT'
+import type { Config } from 'tailwindcss';
+import withMT from '@material-tailwind/react/utils/withMT';
 
 const config: object = withMT({
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {},
-  plugins: [],
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        colors: {
+            primary: {
+                lightness: '#ecfeff', // Cyan 100
+                light: '#22d3ee', // Cyan 400
+                DEFAULT: '#0891b2', // Cyan 600
+                dark: '#155e75', // Cyan 800
+                darkness: '#082f49', // Cyan 950
+            },
+        },
+    },
+    plugins: [],
 } as Config);
 
 export default config;
